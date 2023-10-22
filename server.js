2c955dde51e75bb80a88af12dcef637bdcbf5a78
Shrekboi <85759550+Shrekboi@users.noreply.github.com>
2023-10-22 19:33:57 +0300

    try

--- server.js
+++ server.js
@@ -1,66 +1,61 @@
-// const cors = require('cors');
-// const express = require('express');
-// const puppeteer = require('puppeteer');
-// const app = express();
-// // Port on which your server will listen
+const cors = require('cors');
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
+// Port on which your server will listen
 const PORT = process.env.PORT || 3030;
 
-// // Define a route to serve an HTML page
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
+// Define a route to serve an HTML page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
-// app.use(cors());
+app.use(cors());
 
-// // Define an API endpoint to get a definition
-// app.get('/api/definition/:word', async (req, res) => {
-//     const word = req.params.word;
-//     const definition = await getDefinition(word);
-//     res.json({ definition });
-// });
+// Define an API endpoint to get a definition
+app.get('/api/definition/:word', async (req, res) => {
+    const word = req.params.word;
+    const definition = await getDefinition(word);
+    res.json({ definition });
+});
 
-// async function getDefinition(word) {
-//     const browser = await puppeteer.launch({
-//         headless: true, // will not show the browser
-//         defaultViewport: null,
-//     });
+async function getDefinition(word) {
+    const browser = await puppeteer.launch({
+        headless: true, // will not show the browser
+        defaultViewport: null,
+    });
 
-//     const page = await browser.newPage();
+    const page = await browser.newPage();
 
-//     let url = "https://dictionary.cambridge.org/dictionary/english/" + word;
+    let url = "https://dictionary.cambridge.org/dictionary/english/" + word;
 
-//     await page.goto(url, {
-//         waitUntil: "domcontentloaded",
-//     });
+    await page.goto(url, {
+        waitUntil: "domcontentloaded",
+    });
 
-//     const definition = await page.evaluate(() => {
-//         // Use a more robust selector to find the definition
-//         const definitionElement = document.querySelector('.def.ddef_d.db');
+    const definition = await page.evaluate(() => {
+        // Use a more robust selector to find the definition
+        const definitionElement = document.querySelector('.def.ddef_d.db');
 
-//         if (definitionElement) {
-//             return definitionElement.innerText;
-//         } else {
-//             return "Definition not found.";
-//         }
-//     });
+        if (definitionElement) {
+            return definitionElement.innerText;
+        } else {
+            return "Definition not found.";
+        }
+    });
 
-//     // Close the browser
-//     await browser.close();
+    // Close the browser
+    await browser.close();
 
-//     console.log(definition)
+    console.log(definition)
 
-//     return definition;
-// }
+    return definition;
+}
 
-// getDefinition("cat")
+getDefinition("cat")
 
 
-const express = require("express")
-const app = express()
 
-app.get("/", function (req, res) {
-    res.send("hello my dude")
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
